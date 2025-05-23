3dcc06bf81e8a659f3df4647282f8d8cea0aec85
beastcode <beastcode9999@gmail.com>
2025-05-23 23:01:09 +0930

    add helpers.php function render()

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,18 @@
 <?php 
 
 
-function render() {
+function render($view, data = []) {
 
-    
+    extract($data); // extract the variables
+ 
+    // start output bufferring as temporary container
+    ob_start();
+
+    // include specific file from views
+    require __DIR__ . '/views/' / $view . '.php';
+
+    // get content from output bufferring
+    $content = ob_get_clean();
+
+    require __DIR__ . '/views/layout.php';
 }
