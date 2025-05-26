97c57ef6967cc546658cb74040afc8c394ef2842
s322596 <S322596@students.cdu.edu.au>
2025-05-26 15:58:42 +0930

    Contact page

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController {
     public function about() {
         // route the index.php 
         // require_once __DIR__ . '/../views/home/index.php';
+        echo views_path('home/about.php');
 
         $data = [
             'title' => 'About page',
@@ -31,5 +32,19 @@ class HomeController {
         ];
 
         render('home/about', $data); // location of directory is home/about
-    }    
+    }   
+    
+    public function contact() {
+        // route the index.php 
+        // require_once __DIR__ . '/../views/home/index.php';
+        echo views_path('home/contact.php');
+
+        $data = [
+            'title' => 'Contact page',
+            'message' => 'Welcome to the contact page', 
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero mollitia odio hic magnam quisquam numquam error eaque assumenda qui, nisi cumque, maiores dolores recusandae doloribus temporibus labore molestiae harum exercitationem!',
+        ];
+
+        render('home/contact', $data); // location of directory is home/contact
+    }
 }
