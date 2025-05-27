4798c34505d80316b16f5696a56028466efada3f
Beast Code <beastcode9999@gmail.com>
2025-05-27 16:34:57 +0930

    merge the two routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 
 
 $routes = [
-<<<<<<< HEAD
     'GET' => [
         '' => 'HomeController@index', // home controller with class index
         'about' => 'HomeController@about',
@@ -11,19 +10,11 @@ $routes = [
         'contact' => 'HomeController@contact',
         'user/register' => 'UserController@register',
         'login' => 'UserController@login',
+        'articles' => 'HomeController@article',
     ],
     'POST' => [
          
     ],
-=======
-    '' => 'HomeController@index', // home controller with class index
-    'about' => 'HomeController@about',
-    'user/login' => 'UserController@login', 
-    'contact' => 'HomeController@contact',
-    'user/register' => 'UserController@register',
-    'login' => 'UserController@login',
-    'articles' => 'HomeController@article',
->>>>>>> 77f2e368893e5d2a4411ad34daab083a4fe1ab3c
 ];
 
 // associative arrays of routes
