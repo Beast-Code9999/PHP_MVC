73dfb1fad7e0ec7f662899447286df950dc51c86
beastcode <beastcode9999@gmail.com>
2025-05-29 01:07:22 +0930

    add about us content

--- app/views/home/about.php
+++ app/views/home/about.php
@@ -1,20 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="<?php echo base_url('css/about.css')?>">
 </head>
-<body>
+
+<!-- This goes inside your <main> tag -->
+<div class="about-container">
+    <h1 class="about-title">About Us</h1>
     
-</body>
-</html>
-<h1>
-    <?php echo $title; ?>
-</h1>
-<p>
-   <?php echo $text; ?>
-</p>
-<h2>
-    <?php echo $message; ?>
-</h2>
+    <div class="about-content">
+        <div class="about-text">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores ea voluptatibus, maxime officia consectetur natus accusantium eligendi soluta, dolorum vero eaque sint consequuntur. Beatae assumenda nihil provident facere error non cumque est quo quas quasi ipsum dignissimos inventore, ratione eaque obcaecati expedita voluptatem soluta! Animi pariatur id a odit, sunt cupiditate cum sed obcaecati omnis aspernatur quae facere excepturi similique accusantium harum error mollitia tempore molestias libero fugiat! Eveniet commodi quod animi, eaque veniam tenetur fugiat ipsa qui incidunt vel labore. Odio illo tempora id, quidem consequatur illum esse asperiores iste debitis ipsum ad quasi magnam odit, voluptatibus cumque facilis.</p>
+        </div>
+        
+        <div class="about-image-main">
+            <img src="/placeholder-main.jpg" alt="About us main image">
+        </div>
+    </div>
+    
+    <div class="about-images-bottom">
+        <div class="about-image-left">
+            <img src="/placeholder-left.jpg" alt="About us image 1">
+        </div>
+        
+        <div class="about-image-right">
+            <img src="/placeholder-right.jpg" alt="About us image 2">
+        </div>
+    </div>
+</div>
